f4c8310aeb7152434ff54833beb74191b3ea0740
abdus quadri <asquadri1@gmail.com>
2023-04-17 13:50:51 -0500

    test 2

--- terraform/aws/rds.tf
+++ terraform/aws/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_rds_cluster" "app1-rds-cluster" {
   cluster_identifier      = "app1-rds-cluster"
-  allocated_storage       = 10
+  allocated_storage       = 11
   backup_retention_period = 0
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
@@ -16,7 +16,7 @@ resource "aws_rds_cluster" "app1-rds-cluster" {
 
 resource "aws_rds_cluster" "app2-rds-cluster" {
   cluster_identifier      = "app2-rds-cluster"
-  allocated_storage       = 10
+  allocated_storage       = 11
   backup_retention_period = 1
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
@@ -33,7 +33,7 @@ resource "aws_rds_cluster" "app2-rds-cluster" {
 resource "aws_rds_cluster" "app3-rds-cluster" {
   cluster_identifier      = "app3-rds-cluster"
   allocated_storage       = 10
-  backup_retention_period = 15
+  backup_retention_period = 16
   tags = {
     git_commit           = "079fe74f6b96d887c245664fbd8cf676c92f20e5"
     git_file             = "terraform/aws/rds.tf"
